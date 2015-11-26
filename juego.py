e432a4c65f89223c246c4aeadb7508dd67a7a49a
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-11-26 16:42:17 +0100

    Partida personalizable. Pista e intentos mejorados

--- juego.py
+++ juego.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 import random
+import math
 print "Bienvenido al juego"
-print "Intente adivinar el numero"
-rangoMaximo = 101
-rangoMinimo = 1
+propia = raw_input("Quieres crear tu propia partida?(y/n)")
+if (propia == "y"):
+    rangoMaximo = input(" Introduce el numero maximo: ")
+    rangoMinimo = input(" Introduce el mumero minimo: ")
+else:
+    rangoMaximo = 100
+    rangoMinimo = 1
+for x in range (1,20):
+    if(math.pow(2,x) > rangoMaximo):
+        numero_intentos = x-1
+        break
+    else:
+        numero_intentos = x  	    
+print "Intente adivinar el numero entre " + str(rangoMinimo) + " y " + str(rangoMaximo)
 puntos = 10
 r = random.randrange(rangoMinimo,rangoMaximo)
-numero_intentos = 5
 array=[]
 print "Tienes",numero_intentos,"intentos y " + str(puntos) + " puntos"
 jugando = True
@@ -25,7 +36,8 @@ while jugando:
 		array.append("El numero es mayor que " + str(intento))
 		numero_intentos = numero_intentos-1
 	elif(intento == r):
-		print "El numero era",intento
+		premio = 90
+		print "Enhorabuena!, has ganado " + str(puntos+premio) + " puntos"
 		jugando = False
 			
 	if(numero_intentos < 1):
@@ -41,12 +53,13 @@ while jugando:
 		if(0.3 * random.randrange(1.0,10.0) < 1.0):
 			pista = raw_input("Quieres una pista(y/n): ")
 			if(pista == "y"):
-				print("El numero que buscas esta entre " + str(r-random.randrange(0,numero_intentos*10)) + " y " + str(random.randrange(0,numero_intentos*10)+r))
+				print("El numero que buscas esta entre " + str(r-random.randrange(0,numero_intentos*5)) + " y " + str(random.randrange(0,numero_intentos*5)+r))
 				puntos -=2
 				print("Se te restan 2 puntos, te quedan " + str(puntos) + " puntos")
 	     
 	else:
-		print "Te quedaban solo", numero_intentos,"intentos"
-		premio = 90
-		print "Enhorabuena!, has ganado " + str(puntos+premio) + " puntos"
-		print "El numero era", r
+	    if(numero_intentos != 0):
+		       print "Te quedaban solo", numero_intentos,"intentos"
+	    else:
+	        print "Te quedaste sin intentos"
+	    print "El numero era", r
