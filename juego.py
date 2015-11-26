2a5a6264555b5d6ae9928fd43c1ce698c0861f3f
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-11-26 22:07:46 +0100

    Errores eliminados, puntuacion logica, repeticion de partida

--- juego.py
+++ juego.py
@@ -35,69 +35,106 @@ getch = _Getch()
 
 #Game Start
 print "Bienvenido al juego"
-print("Quieres crear tu propia partida?(y/n)")
-propia = getch()
-if (propia == "y"):
-    rangoMaximo = input("Introduce el numero maximo: ")
-    rangoMinimo = input("Introduce el mumero minimo: ")
-else:
-    rangoMaximo = 100
-    rangoMinimo = 1
-for x in range (1,20):
-    if(math.pow(2,x) > rangoMaximo):
-        numero_intentos = x-1
-        break
-    else:
-        numero_intentos = x  	    
-print "Intente adivinar el numero entre " + str(rangoMinimo) + " y " + str(rangoMaximo)
-puntos = 10
-number = random.randrange(rangoMinimo,rangoMaximo)
-array=[]
-print "Tienes",numero_intentos,"intentos y " + str(puntos) + " puntos"
-jugando = True
-while jugando:
-	intento = input("Numero:")
-	if((intento< rangoMinimo)or(intento > rangoMaximo)):
-                print "Rango equivocado"
-                print "El rango es de",rangoMinimo,"a",rangoMaximo
-		continue
-	elif(intento > number):
-		print "El numero es menor"
-		array.append("El numero es menor que " + str(intento))
-		numero_intentos = numero_intentos-1
-	elif(intento< number):
-		print "El numero es mayor"
-		array.append("El numero es mayor que " + str(intento))
-		numero_intentos = numero_intentos-1
-	elif(intento == number):
-		premio = 90
-		print "Enhorabuena!, has ganado " + str(puntos+premio) + " puntos"
-		jugando = False
+salir = False
+misPuntos = 0
+maqPuntos = 0
+while(not salir):
+	print("Quieres crear tu propia partida?(y/n)")
+	propia = getch()
+	while(True):
+		if(propia == "y"):
+			try:
+				rangoMaximo = input("Introduce el numero maximo: ")
+				rangoMinimo = input("Introduce el mumero minimo: ")
+			except NameError:
+				print "Numero no valido"
+				continue
+			except SyntaxError:
+				print "Numero no introducido"
+				continue
+
+		else:
+		    rangoMaximo = 100
+		    rangoMinimo = 1
+		for x in range (1,20):
+		    if(math.pow(2,x) > rangoMaximo):
+			numero_intentos = x-1
+			break
+		    else:
+			numero_intentos = x  	    
+		print "Intente adivinar el numero entre " + str(rangoMinimo) + " y " + str(rangoMaximo)
+		try:
+			number = random.randrange(rangoMinimo,rangoMaximo)
+		except ValueError:
+			print("Tiene que ser un numero entero")
+			continue
+		array=[]
+		print "Tienes",numero_intentos,"intentos"
+		jugando = True
+		break
+	while jugando:
+	
+		try:
+			intento = int(raw_input("Numero: "))
+		except SyntaxError:
+			print "Numero no introducido"
+			continue
+		except ValueError:
+			print "Numero no introducido"
+			continue
+
+		if((intento< rangoMinimo)or(intento > rangoMaximo)):
+		        print "Rango equivocado"
+		        print "El rango es de",rangoMinimo,"a",rangoMaximo
+			continue
+		elif(intento > number):
+			print "El numero es menor"
+			array.append("El numero es menor que " + str(intento))
+			numero_intentos = numero_intentos-1
+		elif(intento< number):
+			print "El numero es mayor"
+			array.append("El numero es mayor que " + str(intento))
+			numero_intentos = numero_intentos-1
+		elif(intento == number):
+			premio = 1
+			print "Enhorabuena!, has ganado " + str(premio) + " punto"
+			jugando = False
 			
-	if(numero_intentos < 1):
-	    jugando = False
+		if(numero_intentos < 1):
+		    jugando = False
 
-	if(jugando):
-     		print "Resumen:"
-		puntos -= 1
-     		print "Te quedan",numero_intentos,"intentos y " + str(puntos) + " puntos"
+		if(jugando):
+	     		print "Resumen:"
+	     		print "Te quedan",numero_intentos,"intentos"
 
-		for x in array:
-	        	print x
-		if(0.3 * random.randrange(1.0,10.0) < 1.0):
-			print(" Quieres una pista?(y/n)")
-			pista = getch()
-			if(pista == "y"):
-				print("El numero que buscas esta entre " + str(number-random.randrange(0,numero_intentos*5)) + " y " + str(random.randrange(0,numero_intentos*5)+number))
-				puntos -=2
-				print("Se te restan 2 puntos, te quedan " + str(puntos) + " puntos")
-	     
+			for x in array:
+				print x
+			if(0.3 * random.randrange(1.0,10.0) < 1.0):
+				print(" Quieres una pista?(y/n)")
+				pista = getch()
+				if(pista == "y"):
+					print("El numero que buscas esta entre " + str(number-random.randrange(0,numero_intentos*5)) + " y " + str(random.randrange(0,numero_intentos*5)+number))
+					numero_intentos -= 1
+					print("Se te resta 1 intento, te quedan " + str(numero_intentos) + " intentos")
+		     			if(numero_intentos < 1):
+		    				jugando = False
+		if(not jugando):
+		    if(numero_intentos != 0):
+			       print "Te quedaban solo", numero_intentos,"intentos"
+		    else:
+			print "Te quedaste sin intentos"
+			maqPuntos += 1
+		    print "El numero era", number
+	print("Llevas " + str(misPuntos) +" puntos y la maquina lleva " + str(maqPuntos) + " puntos")
+	
+	print("Quieres jugar otra vez?(y/n)")
+	varSalir = getch()
+	if(varSalir == "y"):
+		salir = False
 	else:
-	    if(numero_intentos != 0):
-		       print "Te quedaban solo", numero_intentos,"intentos"
-	    else:
-	        print "Te quedaste sin intentos"
-	    print "El numero era", number
-
-
-
+		salir = True
+		
+if(misPuntos > maqPuntos):
+	print("Has ganado a la maquina")
+else:
+	print("La maquina ha conseguido mas puntos que tu")
